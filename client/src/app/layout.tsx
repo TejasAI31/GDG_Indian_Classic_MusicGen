dc59c282793217099fd1f9a55e03dac950fe0c73
Omesh2004 <omeshmehta70@gmail.com>
2025-03-23 10:06:50 +0530

    admin page scolourscheme changed to dark

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
         >
-          <Navigation />
+          <Navigation/>
           {children}
         </body>
       </html>
